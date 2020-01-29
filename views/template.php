cfc20f50eac132fec417b3e5f190353e87071b6b
TheBdouilleur2 <52212434+TheBdouilleur2@users.noreply.github.com>
2020-01-29 15:28:24 +0100

    utilisation de bootstrap pour la navbar

--- views/template.php
+++ views/template.php
@@ -11,60 +11,54 @@
 <body>
 	<header>
 		<?php require_once('controllers/php/functions.php') ?>
-		<img src="public/Images/logo2.png" alt="Logo des JE">
-		<nav>
-		<ul>
-			<li><a href="index.php"><?php
-				if(isset($_SESSION['state']) && $_SESSION['state'] == 'admin') {
-					echo 'Accueil';?></a>
-					<ul class='hidden' >
-						<li><a href="index.php">Utilisateurs</a></li>
-						<li><a href="index.php?url=admin_space">Administrateurs</a></li>
-					</ul>
-				<?php } 	else{
-							echo 'Accueil';
-				}?>
-			</li>
+		<nav class="navbar navbar-expand-lg navbar-light bg-info">
+		<a class="navbar-brand" href="index.php">
+		    <img src="public/Images/logo2.png" alt="Logo des JE">
+ 		</a>
+		<ul class="navbar-nav mr-auto">
+			<li class='nav_item'><a class="nav-link" href="index.php">Accueil</a></li>
+			<?php
+			if(isset($_SESSION['ID'], $_SESSION['state']) AND $_SESSION['state']=='admin'){?>
+				<li class='nav-item'><a class="nav-link" href="index.php?url=admin_space">Espace admin</a></li>
+			<?php } ?>
 			<!-- <li>Articles</li> -->
-			<li><a href="index.php?url=projects" class="nav-link">Projets en cours</a></li>
-			<li><a href="<?php if(isset($_SESSION['ID'])){echo "index.php?url=chat";}
-				else{ echo 'index.php?url=device';} ?>"> <?php
-					if(isset($_SESSION['state']) && $_SESSION['state'] == 'admin') {
-						echo 'Discussions';?>
-				</a>
-					<ul class='hidden'>
-						<li><a href="index.php?url=chat">Utilisateurs</a></li>
-						<li><a href="index.php?url=chat_admin">Administrateurs</a></li>
-					</ul>
-			<?php } else if(isset($_SESSION['ID'])){
-				echo 'Discussions';
-			}
-			else{
-				echo 'Donner un avis';
-			}?>
-			</li>
+			<li class='nav-item'><a class="nav-link" href="index.php?url=projects" class="nav-link">Projets en cours</a></li>
+			<?php if(isset($_SESSION['ID']) AND $_SESSION['state']=='admin'){ ?>
+				<li class="nav-item dropdown">
+        				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          					Discussions
+        				</a>
+        				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          					<a class="dropdown-item" href="#">Discussion utilisateur</a>
+						<a class="dropdown-item" href="#">Discussion admin</a>
+        				</div>
+      				</li>
+			<?php } else if(isset($_SESSION['ID'])){ ?>
+				<li class='nav-item'><a class="nav-link" href='index.php?url=chat'>Discussion</a></li>
+			<?php } else{ ?>
+				<li class='nav-item'><a class="nav-link" href='index.php?url=show'>Donner un avis</a></li>
+			<?php }?>
+			
 		</ul>
-	</nav>
-	
-	<div>
-	 	<?php
-	 		$date = getTheDate();
-	 		echo $date;
-		?>
-	</div>
-	<div class='icones spacebetween'>
+			<div class="navbar-brand">
+	 		<?php
+	 			$date = getTheDate();
+	 			echo $date;
+			?>
+		</div>
+		<div class='icones spacebetween'>
 		<?php if(isset($_SESSION['ID'])){ ?>
-		<a href="index.php?url=edit_profil">
+			<a cl=lass='nav-link' href="index.php?url=edit_profil">
 			<div class="edition">
 				<?=$_SESSION['username']?>
 			</div>
-		</a>
+			</a>
 		<div>
 			<a href="index.php?url=deconnection">
 			<img src="public/Images/deconnection.png" alt="Deconnection"></a>
 		</div>
-	 <?php }else{			 ?>
-		<a href="index.php?url=sign_in">
+	 	<?php }else{?>
+			<a href="index.php?url=sign_in">
 			<div class="sign">
 				<img src="public/Images/sign-in.png" alt="Sign-in">
 			</div>
@@ -76,6 +70,8 @@
 		</a>
 		<?php } ?>
 	</div>
+	</nav>
+	
 	</header>
 
 <section>
