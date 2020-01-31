f18afe06ee692af8aadb4dfc113a93fbd39e6bae
TheBdouilleur2 <jullien.rien67@gmail.com>
2020-01-31 11:45:53 +0000

    Ajout de l'affichage des tags pour les projets

--- views/projectsView.php
+++ views/projectsView.php
@@ -3,11 +3,16 @@
 <?php ob_start(); ?>
 
 <!-- La page d'affichage des projets -->
-<?php while ($projects) { ?>
+<?php while ($projects) { 
+    $tags = explode($project['tags'], "/");
+    ?>
     <a href='index.php?url=project/<?=$project['ID'] ?>' class='unlink'>
 			<div class="project">
 				<p><strong><?=$project['title']?></strong>  publié le <?=$project['date_fr']?> par <strong>@<?=$project['creator']?></strong></p>
 				<p><?=nl2br($project['summary'])?></p>
+                <?php foreach ($tags as  $tag) { ?>
+                    <span class="badge"><?=$tag?></span>
+                <?php } ?>
 				<?php if (isset($_SESSION['state']) && $_SESSION['state']=="admin") {?>
 					<a href="index.php?url=edit_project/<?=$project['ID']?>" class='button'>Modifier le projet.</a>
 	<?php	} ?>
