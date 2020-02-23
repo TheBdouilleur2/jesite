59549a9887471d1e6f69b722043760debb068df7
jcode <jcode@gmail.com>
2020-02-23 19:42:25 +0100

    Modif des info de connection à la db

--- models/Manager.php
+++ models/Manager.php
@@ -5,7 +5,7 @@
 class Manager
 {
   protected function dbConnect(){
-    $db = new PDO('mysql:host=localhost;dbname=jesite;charset=utf8', 'root', '',  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $db = new PDO('mysql:host=localhost;dbname=jesite2;charset=utf8', 'user', 'user123#',  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     return $db;
   }
 }
