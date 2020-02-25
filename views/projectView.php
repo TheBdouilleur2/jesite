d4823a37b0408830b90351586e28166f760351b5
jcode <jcode@gmail.com>
2020-02-25 21:36:54 +0100

    Petit réglages de l'affichege des tags pour un projet

--- views/projectView.php
+++ views/projectView.php
@@ -4,12 +4,13 @@
 
 <!-- La page d'affichage d'un projet' -->
 <h1><?=$project['title']?></h1>
-    <p>Publié le <?=$project['date_fr']?> par <strong>@<?=$project['creator']?></strong><?php foreach ($tags as $tag) { ?>
-        <span class="badge"><?$tag?></span>
+    <p>Publié le <?=$project['date_fr']?> par <strong>@<?=$project['creator']?></strong> <?php 
+    foreach($tags as $k => $tag) {?>
+        <span class="badge badge-secondary"><?= $tag ?></span>
     <?php } ?></p>
     <?php if (isset($_SESSION['state']) && $_SESSION['state']=="admin") {?>
-					<a href="index.php?url=edit_project/<?=$project['ID']?>" class='button'>Modifier le projet.</a>
-                    <a href="index?url=delete_project/<?=$project['ID']?>" class='button'>Suprimer le projet.</a>
+					<a href="index.php?url=edit_project/<?=$project['ID']?>" class='btn btn-primary'>Modifier le projet.</a>
+                    <a href="index?url=delete_project/<?=$project['ID']?>" class='btn btn-primary'>Suprimer le projet.</a>
 	<?php	} ?>
     <?= $content ?>
 <br>
--- views/template.php
+++ views/template.php
@@ -7,6 +7,9 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel='shortcut icon' href="public/Images/Pythonsign.ico">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 <body>
 	<header>
@@ -25,7 +28,7 @@
 			<li class='nav-item'><a class="nav-link" href="index.php?url=projects" class="nav-link">Projets en cours</a></li>
 			<?php if(isset($_SESSION['ID']) AND $_SESSION['state']=='admin'){ ?>
 				<li class="nav-item dropdown">
-        				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        				<a class="nav-link dropdown-toggle" href="index.php?url=chats" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
           					Discussions
         				</a>
         				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
