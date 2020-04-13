fa3a26994adcdad2edd56a7d17855be6663ff1ab
TheBdouilleur2 <jcodeje@protonmail.com>
2020-04-13 18:17:51 +0200

    Mise en place du markdown dans le chat

--- controllers/php/async/sendMsg.php
+++ controllers/php/async/sendMsg.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 require_once($_SERVER['DOCUMENT_ROOT'] . "/models/ChatManager.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/controllers/php/Parsedown.php");
+
+$Parsedown = new Parsedown();
+$Parsedown->setSafeMode(true);
 
 $chatManager = new ChatManager();
 $success = 0;
@@ -9,7 +13,10 @@ $msg = "Une erreur est survenue (script.php)";
 if (!empty($_POST['msg'])) {
 	$msg = htmlspecialchars(strip_tags($_POST['msg']));
 	
-	// TODO: ajouter le traitement des liens et du markdown
+	// TODO: ajouter le traitement des liens
+
+	$msg = $Parsedown->text($msg);
+
 	if($_POST['category'] === 'user'){
 		$chatManager->postUserMessage($msg, $_SESSION['ID']);
 		$success = 1;
