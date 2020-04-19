74df0b5f80b0651f50eaad1e5bdc505030445073
TheBdouilleur2 <jcodeje@protonmail.com>
2020-04-19 14:25:10 +0200

    Ajout des trois derniers projets sur la page d'accueil.

--- views/projects/projectView.php
+++ views/projects/projectView.php
@@ -9,14 +9,13 @@
             <span class="badge badge-secondary"><?= $tag ?></span>
     <?php } ?></p>
     <div class="col">
-    <?php if (isset($_SESSION['state']) && $_SESSION['state']=="admin") {?>
-        <a href="/edit_project/<?=$project['ID']?>" class='btn btn-success btn-sm'>Modifier le projet.</a>
-        <a href="/delete_project/<?=$project['ID']?>" class='btn btn-danger btn-sm'>
+        <?php if((isset($_SESSION['state']) && $_SESSION['state']=="admin") || (isset($_SESSION['ID']) && $_SESSION['username'] === $project['creator'])){?>
+            <a href="/edit_project/<?=$project['ID']?>" role='button' class='btn btn-sm btn-success' >Modifier le projet.</a>
+            <a href="/delete_project/<?=$project['ID']?>" role='button' class='btn btn-sm btn-danger' >
             <svg class="bi bi-trash-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"/>
-            </svg>
-        </a>
-    <?php	} ?>
+            <path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"/></svg>
+            </a>
+        <?php } ?>
     </div>
 </div>
 <br>
