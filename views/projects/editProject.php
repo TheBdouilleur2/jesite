da631b2253dca7f61be822da1d2944fb09e5e6d0
TheBdouilleur2 <jcodeje@protonmail.com>
2020-04-23 14:44:11 +0200

    Supression de l'ajax inutile pour les projets

--- views/projects/editProject.php
+++ views/projects/editProject.php
@@ -1,29 +1,34 @@
 <?php ob_start(); ?>
 <!-- La page de modification d'un projet -->
 
+<?php if(isset($_SESSION['error']) && !empty($_SESSION['error'])){?>
+      <div class="alert alert-danger" role="alert">
+      <?=$_SESSION['error']?>
+      </div>
+<?php }?>
+
 <p>Les champs marqué d'une * sont à renseigner obligatoirement.</p>
-<form id="project_edition">
+<form id="project_edition" method="POST" action="/set_project">
     <div class="from-group">
         <label for="newtitle">Titre*:</label>
         <input class="form-control" type="text" placeholder="Votre titre" id="newtitle" name="newtitle" value="<?=$project['title']?>" />
     </div>
     <div class="from-group">
-        <label for="newcontent">Contenu*:</label>
-        <textarea class="form-control" placeholder="Votre contenu" id="newcontent" name="newcontent" ><?=nl2br($project['content'])?></textarea>
+        <label for="newtags">Tags:</label>
+        <input class="form-control" type="text" id="newtags" name='newtags' value="<?=$project['tags']?>">
     </div>
     <div class="from-group">
         <label for="newsummary">Resumé*:</label>
         <textarea class="form-control" placeholder="Votre resumé" id="newsummary" name="newsummary" ><?= nl2br($project['summary'])?></textarea>   
     </div>
     <div class="from-group">
-        <label for="newtags">Tags:</label>
-        <input class="form-control" type="text" id="newtags" name='newtags' value="<?=$project['tags']?>">
+        <label for="newcontent">Contenu*:</label>
+        <textarea class="form-control" placeholder="Votre contenu" id="newcontent" name="newcontent" ><?=nl2br($project['content'])?></textarea>
     </div>
     <input type="text" hidden name='id' id='id' value="<?=$project['ID']?>">
     <input type="submit" name="formedition" value="Sauvegarder les modifications" />  
 </form>
 
-<script src="/public/js/editProject.js"></script>
 
 <?php $content = ob_get_clean();?>
 <?php require_once(ROOT . '/views/templates/template.php');?>
--- views/projects/newprojectView.php
+++ views/projects/newprojectView.php
@@ -4,29 +4,34 @@
 
 <!-- La page de création d'un projet -->
 
+<?php if(isset($_SESSION['error']) && !empty($_SESSION['error'])){?>
+      <div class="alert alert-danger" role="alert">
+      <?=$_SESSION['error']?>
+      </div>
+<?php }?>
+
 <p>Les champs marqués d'une * sont à renseigner obligatoirement.</p>
-<form method="POST" id="creation_project">
+<form action="/create_project" method="POST" id="creation_project">
    <div class="form-group">
       <label for="project_title">Titre*:</label>
-      <input class="form-control" type="text" placeholder="Votre titre" id="project_title" name="project_title" />
+      <input class="form-control" type="text" placeholder="Votre titre" id="project_title" name="project_title" value="<?php if(isset($_SESSION['project_info']['project_title'])){echo $_SESSION['project_info']['project_title'];}?>"/>
    </div>
    <div class="form-group">
       <label for='tags'>Entrer ici les tags séparés par des / :</label>
-      <input class="form-control" type="text" placeholder="tag1/tag2/..." id="tags" name="tags" value="<?php if(isset($tags)) { echo $tags; } ?>" />
+      <input class="form-control" type="text" placeholder="tag1/tag2/..." id="tags" name="tags" value="<?php if(isset($_SESSION['project_info']['tags'])){echo $_SESSION['project_info']['tags'];}?>" />
    </div>
    <div class="form-group">
       <label for="summary">Resumé*:</label>
-      <textarea class="form-control" placeholder="Votre resumé" id="summary" name="summary"></textarea>
+      <textarea class="form-control" placeholder="Votre resumé" id="summary" name="summary"><?php if(isset($_SESSION['project_info']['summary'])){echo $_SESSION['project_info']['summary'];}?></textarea>
    </div>
    <div class="form-group">
       <label for="project_content">Corps du projet*:</label>
-      <textarea class="form-control" placeholder="Votre contenu" id="project_content" name="project_content"></textarea>
+      <textarea class="form-control" placeholder="Votre contenu" id="project_content" name="project_content"><?php if(isset($_SESSION['project_info']['project_content'])){echo $_SESSION['project_info']['project_content'];}?></textarea>
    </div>
    <input type="submit" name="creationform" value="Création du projet" />
 </form>
-<p id="error" style="color: red;"><?php if(isset($error)){echo $error;} ?></p>
 
-<script src="public/js/newProject.js"></script>
+<!-- <script src="public/js/newProject.js"></script> -->
 
 <?php $content = ob_get_clean();?>
 <?php require_once(ROOT . '/views/templates/template.php');?>
