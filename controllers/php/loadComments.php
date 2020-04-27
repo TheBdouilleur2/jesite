de7d1dc6b9a637f31aba67cde8af7e2c7d663dae
TheBdouilleur2 <jcodeje@protonmail.com>
2020-04-27 20:56:05 +0200

    Mise en place du rechargement des commentaires

--- controllers/php/loadComments.php
+++ controllers/php/loadComments.php
@@ -1,15 +1,15 @@
 <?php
-
+session_start();
 require_once($_SERVER['DOCUMENT_ROOT'] . '/models/CommentsManager.php');
 
 $CommentsManager = new CommentsManager;
 
-//TODO récuperer l'id du projet
+if($_SESSION['loadCommentsProjectId']){
+	$comments = $CommentsManager->getCommentsByProject((int)$_SESSION['loadCommentsProjectId']);
 
-$comments = $CommentsManager->getCommentsByProject(14);
-
-foreach($project["comments"] as $comment){ ?>
-    <div class="comment bord">
-			<p><a href="/profile/<?=$comment['sender_id']?>" class="unlike"><strong>@<?php echo htmlspecialchars($comment['sender']); ?></strong></a>[<?php echo $comment['age'];?>]: <?=$comment['msg']?></p>
-	</div>
-<?php }
+	foreach($comments as $comment){ ?>
+		<div class="comment">
+				<p><a href="/profile/<?=$comment['sender_id']?>" class="unlike"><strong>@<?php echo htmlspecialchars($comment['sender']); ?></strong></a>[<?php echo $comment['age'];?>]: <?=$comment['msg']?></p>
+		</div>
+	<?php }
+}
