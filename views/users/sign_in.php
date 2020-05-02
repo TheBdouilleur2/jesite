0426dc4ad99e8639de2ea7ab81c7dad9511fb21f
TheBdouilleur2 <jcodeje@protonmail.com>
2020-05-02 20:00:37 +0200

    Amélioration du style des formulaires de connections.
    Je me suis beaucoup amusé!

--- views/users/sign_in.php
+++ views/users/sign_in.php
@@ -1,43 +1,30 @@
 
-<?php if(isset($_SESSION['error']) && !empty($_SESSION['error'])){?>
-   <div class="alert alert-danger" role="alert">
-   <?=$_SESSION['error']?>
-   </div>
-<?php }?>
 
-   <p>Veuillez vous connecter.</p>
-   	<form action="/connect_user" method="POST" id="sign-in">
-       <table>
-            <tr>
-               <td align="right">
-                  <label for="username_connect">Pseudo :</label>
-               </td>
-               <td>
-                  <input autofocus type="text" placeholder="Votre pseudo" id="username_connect" name="username_connect" maxlenght='20' value="<?php if(isset($username)) { echo $username; } ?>" />
-               </td>
-            </tr>
-            <tr>
-               <td align="right">
-                  <label for="passwd_connect">Mot de passe :</label>
-               </td>
-               <td>
-                  <input type="password" placeholder="Votre mot de passe" id="passwd_connect" name="passwd_connect" />
-               </td>
-            </tr>
-            <tr>
-               <td align="right">
-                  <input type="checkbox" name="rememberme" id="rememberme">
-               </td>
-               <td>
-                  <label for="rememberme">Se souvenir de moi</label>
-               </td>
-            </tr>
-            <tr>
-               <td></td>
-               <td align="center">
-                  <br />
-                  <input type="submit" name="formconnection" value="Je me connecte" />
-               </td>
-            </tr>
-         </table>
-   	</form>
+<div class="row d-flex justify-content-center">
+  <div class="col">
+    <form class="bord" method="POST" action="/connect_user">
+      <?php if(isset($_SESSION['error']) && !empty($_SESSION['error'])){?>
+          <div class="alert alert-danger" role="alert">
+          <?=$_SESSION['error']?>
+          </div>
+      <?php }?>
+      <h1>Connexion</h1>
+      <div class="form-group">
+      <label for="username_connect">Pseudo</label>
+      <input autofocus type="text" name="username_connect" id="username_connect" class="form-control" placeholder="Votre pseudo" aria-describedby="helpId">
+      </div>
+      <div class="form-group">
+        <label for="passwd_connect">Mot de passe</label>
+        <input type="password" class="form-control" name="passwd_connect" id="passwd_connect" placeholder="Votre mot de passe">
+      </div>
+      <div class="form-check">
+        <label class="form-check-label" for="rememderme">
+          <input type="checkbox" name="rememderme" id="rememderme" value="checkedValue"><span class="checkmark"></span>
+          Se souvenir de moi
+        </label>
+      </div>
+
+      <input type="submit" name="formconnection" value="Je me connecte" />
+    </form>
+  </div>
+</div>
--- views/users/sign_up.php
+++ views/users/sign_up.php
@@ -1,61 +1,39 @@
 
-<?php if(isset($_SESSION['error']) && !empty($_SESSION['error'])){?>
-   <div class="alert alert-danger" role="alert">
-   <?=$_SESSION['error']?>
-   </div>
-<?php }?>
 
-   <p>Veuillez créer un compte.</p>
-   	<p>Les champs marqués d'une * sont à renseigner obligatoirement.</p>
-      <form action="/create_user" method="POST" id="sign-up">
-       <table>
-            <tr>
-               <td align="right">
-                  <label for="username">Pseudo* :</label>
-               </td>
-               <td>
-                  <input autofocus type="text" placeholder="Votre pseudo" id="username" name="username" maxlenght='20' value="<?php if(isset($username)) { echo $username; } ?>" />
-               </td>
-            </tr>
-            <tr>
-               <td align="right">
-                  <label for="mail">Mail :</label>
-               </td>
-               <td>
-                  <input type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
-               </td>
-            </tr>
-            <tr>
-               <td align="right">
-                  <label for="passwd">Mot de passe* :</label>
-               </td>
-               <td>
-                  <input type="password" placeholder="Votre mot de passe" id="passwd" name="passwd" />
-               </td>
-            </tr>
-            <tr>
-               <td align="right">
-                  <label for="passwd2">Confirmation du mot de passe* :</label>
-               </td>
-               <td>
-                  <input type="password" placeholder="Confirmez votre mot de passe" id="passwd2" name="passwd2" />
-               </td>
-            </tr>
-            <tr>
-               <td align="right">
-                  <input type="checkbox" name="rememberme" id="rememberme">
-               </td>
-               <td>
-                  <label for="rememberme">Se souvenir de moi</label>
-         </td>
-      </tr>
-         <tr>
-         <td></td>
-         <td align="center">
-            <br />
-            <input type="submit" name="formregistration" value="Je m'inscris" />
-         </td>
-      </tr>
-   </table>
-</form>
-
+<div class="row d-flex justify-content-center">
+  <div class="col">
+    <form action="/create_user" method="POST"class="bord">
+      <?php if(isset($_SESSION['error']) && !empty($_SESSION['error'])){?>
+          <div class="alert alert-danger" role="alert">
+          <?=$_SESSION['error']?>
+          </div>
+      <?php }?>
+      <h1>Creer un compte</h1>
+      <p>Les champs marqués d'une * sont à renseigner obligatoirement.</p>
+      <div class="form-group">
+        <label for="username">Pseudo*:</label>
+        <input autofocus type="text" name="username" id="username" class="form-control" placeholder="Votre pseudo" aria-describedby="helpId">
+        <small id="helpId" class="text-muted">Le pseudo ne doit pas dépasser 20 caractères</small>
+      </div>
+      <div class="form-group">
+        <label for="mail">Mail:</label>
+        <input type="email" name="mail" id="mail" class="form-control" placeholder="Votre mail" aria-describedby="helpId">
+      </div>
+      <div class="form-group">
+        <label for="passwd">Mot de passe* :</label>
+        <input type="password" class="form-control" name="passwd" id="passwd" placeholder="Votre mot de passe">
+      </div>
+      <div class="form-group">
+        <label for="passwd2">Confirmation du mot de passe* :</label>
+        <input type="password" class="form-control" name="passwd2" id="passwd2" placeholder="Confirmez votre mot de passe">
+      </div>
+      <div class="form-check">
+        <label class="form-check-label" for="rememderme">
+          <input type="checkbox" name="rememderme" id="rememderme" value="checkedValue"><span class="checkmark"></span>
+          Se souvenir de moi
+        </label>
+      </div>
+      <input type="submit" name="formregistration" value="Je m'inscris" />
+    </form>
+  </div>
+</div>
