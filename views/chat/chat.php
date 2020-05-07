5cf16d3a6ab3003789dab811e25032b610fa81f0
TheBdouilleur <bdouilleur@gmail.com>
2020-05-07 16:58:02 +0200

    Added link and on hover text to MD logo

--- views/chat/chat.php
+++ views/chat/chat.php
@@ -5,7 +5,9 @@
         <div class="container d-flex justify-content-end">
             <div class="row d-flex justify-content-end">
                 <div class="col">
-                <svg xmlns="http://www.w3.org/2000/svg" width="108" height="18" viewBox="0 0 208 128"><rect width="198" height="118" x="5" y="5" ry="10" stroke="#000" stroke-width="10" fill="none"/><path d="M30 98V30h20l20 25 20-25h20v68H90V59L70 84 50 59v39zm125 0l-30-33h20V30h20v35h20z"/></svg>
+		    <a href="https://www.markdownguide.org/cheat-sheet/" title="Tuto Markdown">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="108" height="18" viewBox="0 0 208 128"><rect width="198" height="118" x="5" y="5" ry="10" stroke="#000" stroke-width="10" fill="none"/><path d="M30 98V30h20l20 25 20-25h20v68H90V59L70 84 50 59v39zm125 0l-30-33h20V30h20v35h20z"/></svg>
+		    </a>
                 </div>
             </div>
         </div>
