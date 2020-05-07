ee47d4c8782b59636ecf46c30b1a8ca28c53a444
TheBdouilleur <bdouilleur@gmail.com>
2020-05-07 16:44:59 +0200

    Deleted old link, added link and text to MD icon

--- views/projects/editProject.php
+++ views/projects/editProject.php
@@ -6,7 +6,7 @@
       </div>
 <?php }?>
 
-<p>Les champs marqués d'une * sont à renseigner obligatoirement. Le <a href=https://fr.wikipedia.org/wiki/Markdown>Markdown</a> est pris en charge (voir <a href=https://www.markdownguide.org/cheat-sheet/ >tuto rapide</a>). </p>
+<p>Les champs marqués d'une * sont à renseigner obligatoirement.</p>
 <form id="project_edition" method="POST" action="/set_project">
     <div class="from-group">
         <label for="newtitle">Titre*:</label>
@@ -14,7 +14,9 @@
         <div class="container d-flex justify-content-end">
             <div class="row d-flex justify-content-end">
                 <div class="col">
-                <svg xmlns="http://www.w3.org/2000/svg" width="108" height="18" viewBox="0 0 208 128"><rect width="198" height="118" x="5" y="5" ry="10" stroke="#000" stroke-width="10" fill="none"/><path d="M30 98V30h20l20 25 20-25h20v68H90V59L70 84 50 59v39zm125 0l-30-33h20V30h20v35h20z"/></svg>
+                    <a href="https://www.markdownguide.org/cheat-sheet/" title="Tuto Markdown">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="108" height="18" viewBox="0 0 208 128"><rect width="198" height="118" x="5" y="5" ry="10" stroke="#000" stroke-width="10" fill="none"/><path d="M30 98V30h20l20 25 20-25h20v68H90V59L70 84 50 59v39zm125 0l-30-33h20V30h20v35h20z"/></svg>
+                    </a>
                 </div>
             </div>
         </div>
