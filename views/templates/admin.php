b6f18377a6bd6fd30b9470c5e07128388e86166d
TheBdouilleur2 <jcodeje@protonmail.com>
2020-05-15 09:06:25 +0200

    Simplification du code des templates.
    Il reste le problème de la page d'acceuil.

--- views/templates/admin.php
+++ views/templates/admin.php
@@ -4,71 +4,47 @@
 	<meta charset="UTF-8">
 	<title><?= $title ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Ajout du style: -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="/public/style/style.css">
 	<link rel='shortcut icon' href="/public/Images/Pythonsign.ico">
-	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"><!-- TODO ajouter la police roboto -->
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
 <body>
 	<header>
-		<?php require_once('controllers/php/functions.php') ?>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<a class="navbar-brand" href="/index.php">
 		    <img src="/public/Images/logo.png" alt="Logo des JE">
  		</a>
 		<ul class="navbar-nav mr-auto">
 			<li class='nav_item'><a class="nav-link" href="/index.php">Accueil</a></li>
-			<?php
-			if(isset($_SESSION['ID'], $_SESSION['state']) AND $_SESSION['state']=='admin'){?>
-				<li class='nav-item'><a class="nav-link" href="/admin">Espace admin</a></li>
-			<?php } ?>
+			<li class='nav-item'><a class="nav-link" href="/admin">Espace admin</a></li>
 			<!-- <li>Articles</li> -->
 			<li class='nav-item'><a class="nav-link" href="/projects" class="nav-link">Projets</a></li>
-			<?php if(isset($_SESSION['ID']) AND $_SESSION['state']=='admin'){ ?>
-				<li class="nav-item dropdown">
-        				<a class="nav-link dropdown-toggle" href="/chats" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          					Discussions
-        				</a>
-        				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          					<a class="dropdown-item" href="/chat">Discussion utilisateur</a>
-						<a class="dropdown-item" href="/admin/chat">Discussion admin</a>
-        				</div>
-      				</li>
-			<?php } else if(isset($_SESSION['ID'])){ ?>
-				<li class='nav-item'><a class="nav-link" href='/chat'>Discussion</a></li>
-			<?php } else{ ?>
-				<!-- <li class='nav-item'><a class="nav-link" href='/show'>Donner un avis</a></li> -->
-			<?php }?>
-			
-			</ul>
-				<div class="navbar-brand">
-				<?php
-					$date = getTheDate();
-					echo $date;
-				?>
+			<li class="nav-item dropdown">
+        		<a class="nav-link dropdown-toggle" href="/chats" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+          			Discussions
+        		</a>
+    			<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					<a class="dropdown-item" href="/chat">Discussion utilisateur</a>
+					<a class="dropdown-item" href="/admin/chat">Discussion admin</a>
+    			</div>
+      		</li>
+		</ul>
+				<div class="navbar-brand d-none d-md-block">
+				<?= $date ?>
 			</div>
 			<div>
-			<?php if(isset($_SESSION['ID'])){ ?>
-				<div class='navbar-brand nav-link dropdown' href="/profile">
-					<span class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="userMenu"><?=$_SESSION['username']?></span>
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userMenu">
-						<a href="/account" class="dropdown-item">Mon compte</a>
-						<a href="/deconnection" class="dropdown-item">Déconnexion</a>
-					</div>
+			<div class='navbar-brand nav-link dropdown' href="/profile">
+				<span class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="userMenu"><?=$_SESSION['username']?></span>
+				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userMenu">
+					<a href="/account" class="dropdown-item">Mon compte</a>
+					<a href="/deconnection" class="dropdown-item">Déconnexion</a>
 				</div>
-			<?php }else{?>
-				<a href="/sign_in">
-				<div class="sign">
-					<img src="/public/Images/sign-in.png" alt="Sign-in">
-				</div>
-				</a>
-				<a href="/sign_up">
-					<div class="sign">
-						<img src="/public/Images/sign-up.png" alt="Sign-up">
-					</div>
-				</a>
-			<?php } ?>
+			</div>
 		</div>
 	</nav>
 	
